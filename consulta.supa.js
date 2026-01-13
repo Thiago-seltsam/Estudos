039f31d0bbef92e9b16c21e2d88df072fcf5eb58
Thiago-seltsam <thiagotst.rj12@gmail.com>
2026-01-12 22:33:50 -0300

    Create consulta.supa.js

---
+++ consulta.supa.js
@@ -0,0 +1,17 @@
+fetch("https://luabfnznxvgecovukmpd.supabase.co",{
+    
+    method:"GET",
+    headers:{
+        "x-api-key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1YWJmbnpueHZnZWNvdnVrbXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNzg1MzksImV4cCI6MjA3OTg1NDUzOX0.sc4W-ySg3yHMsEJlIPjT_0-h13Yg7OWCEc661VWU1sw"
+    }
+}
+)
+
+.then(function(response){
+    console.log(response.status);
+    return response.json();
+})
+
+.then(function(data){
+    console.log(data);
+})
