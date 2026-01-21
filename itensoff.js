4d049c28f2e3f04543b173935bb34e8239ed85f8
Thiago-seltsam <thiagotst.rj12@gmail.com>
2026-01-21 00:39:33 -0300

    Att projeto

--- itensoff.js
+++ itensoff.js
@@ -1,4 +1,6 @@
 // CONFIGURAÇÃO SUPABASE
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+
 const URL = "https://luabfnznxvgecovukmpd.supabase.co/rest/v1/ItensOffshore";
 const headers = { 
   "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1YWJmbnpueHZnZWNvdnVrbXBkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDI3ODUzOSwiZXhwIjoyMDc5ODU0NTM5fQ.vFJ7kmVk6qsiuGv9MOLSUCOS_8XrOBaWUcH6Kn4yG0",
