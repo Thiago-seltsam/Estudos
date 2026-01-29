855e0105baaea9468274db8f6ec925c7336a2e27
ThiagoDEVRJ <thiagotst.rj12@gmail.com>
2026-01-28 22:48:55 -0300

    Update API URL and key to placeholders
    Replaced sensitive API URL and key with placeholders.

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
-const URL = "https://luabfnznxvgecovukmpd.supabase.co/rest/v1/trabalho?";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1YWJmbnpueHZnZWNvdnVrbXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNzg1MzksImV4cCI6MjA3OTg1NDUzOX0.sc4W-ySg3yHMsEJlIPjT_0-h13Yg7OWCEc661VWU1sw";
+const URL = "SUA_URL";
+const key = "CHAVE_API";
 
-// Cabeçalhos padrão
+// ============= CABEÇALHO PADRÃO =================== 
 function getHeader() {
     return {
         "Content-Type": "application/json",
